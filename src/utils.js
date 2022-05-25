c109a683d240812af44b1670d86f0d42f12f63d7
Stefano Mitello <stefano@mono.studio>
2022-05-25 09:18:57 +0200

    feat(added paginator logic)

--- src/utils.js
+++ src/utils.js
@@ -55,3 +55,22 @@ export const toLocalStorage = (key = 'data', object) => {
 export const fromLocalStorage = (key = 'data') => {
     return JSON.parse(localStorage.getItem(key));
 }
+
+export function paginator(items, page, per_page) {
+
+    var page = page || 1,
+        per_page = per_page || 10,
+        offset = (page - 1) * per_page,
+
+        paginatedItems = items.slice(offset).slice(0, per_page),
+        total_pages = Math.ceil(items.length / per_page);
+    return {
+        page: page,
+        per_page: per_page,
+        pre_page: page - 1 ? page - 1 : null,
+        next_page: (total_pages > page) ? page + 1 : null,
+        total: items.length,
+        total_pages: total_pages,
+        data: paginatedItems
+    };
+}
